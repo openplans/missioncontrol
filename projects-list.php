8c0c5e876f9297236b42671ee0c08e9632a9ebda
Andy Cochran <acochran@openplans.org>
2012-06-05 19:45:44 -0400

    add person count to projects

--- projects-list.php
+++ projects-list.php
@@ -12,7 +12,22 @@
 
       $row[$i]['project'] = $res['project']; 
 
-      ?><li class="project id-<?php echo $res['project_id']; ?>"><a href="#" class="star">&#10029;</a> <?php echo $res['project']; ?> <span class="team-count">(#)</span> <a href="#" class="delete">&times;</a></li>
+      ?><li class="project id-<?php echo $res['project_id']; ?>">
+          <a href="#" class="star">&#10029;</a> 
+          <?php echo $res['project']; ?> 
+          <?php
+          $the_project_id = $res['project_id'];
+          $rows = $db->query("SELECT count(*) FROM links WHERE project_id = '$the_project_id'");
+          $row = $rows->fetchArray();
+          $numRows = $row['count(*)'];
+          echo '<span class="team-count">(' . $numRows . ')</span>';
+          ?>
+          <form class="delete-project" action="delete-project.php" method="post">
+            <input type="hidden" name="person_count" value="<?php echo $person_count; ?>" />
+            <input type="hidden" name="project_id" value="<?php echo $project_res['project_id']; ?>" />
+            <input type="submit" class="delete" value="&times;" />
+          </form>
+        </li>
 
       <?php
 
